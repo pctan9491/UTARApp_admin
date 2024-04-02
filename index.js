ded9a39c10af66409eb891d9a2b40fa930ef2acd
pctan9491 <82195853+pctan9491@users.noreply.github.com>
2024-04-02 15:31:55 +0800

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
-const bcrypt = require("bcryptjs");
+const cors = require("cors")({ origin: true });
 
 admin.initializeApp();
 
